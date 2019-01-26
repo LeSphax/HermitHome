ea2c221ea3f52c5b672708dca031e8060b197e33
Sebastien Kerbrat <sbkerbrat@gmail.com>
2019-01-26 18:31:21 +0100

    Crab working with two arms

---
+++ Assets/ArmMovement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class ArmMovement : MonoBehaviour
+{
+
+    public float speed = 1000f;
+
+    public GameObject fullArm;
+
+    public KeyCode upCode;
+    public KeyCode downCode;
+    private Rigidbody arm;
+    private Rigidbody forearm;
+    private GrabGround grabGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arm = fullArm.transform.Find("Arm").GetComponent<Rigidbody>();
+        forearm = fullArm.transform.Find("Forearm").GetComponent<Rigidbody>();
+        grabGround = forearm.GetComponent<GrabGround>();
+        grabGround.downCode = downCode;
+        HingeJoint armJoint = gameObject.AddComponent<HingeJoint>();
+        armJoint.connectedBody = arm;
+        armJoint.anchor = transform.InverseTransformPoint(arm.transform.position);
+        armJoint.axis = Vector3.forward;
+        armJoint.useLimits = true;
+        JointLimits l = new JointLimits();
+        l.min = -90;
+        l.max = 0;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(upCode))
+        {
+            arm.AddRelativeTorque(Vector3.back * speed, ForceMode.VelocityChange);
+
+        }
+        else if (Input.GetKey(downCode))
+        {
+            if (grabGround.tempHinge == null)
+            {
+                arm.AddRelativeTorque(Vector3.forward * speed, ForceMode.VelocityChange);
+            }
+        }
+        else
+        {
+            arm.velocity = Vector3.zero;
+            arm.angularVelocity = Vector3.zero;
+            forearm.velocity = Vector3.zero;
+            forearm.angularVelocity = Vector3.zero;
+        }
+    }
+}
--- Assets/GrabGround.cs
+++ Assets/GrabGround.cs
@@ -2,16 +2,41 @@
 
 public class GrabGround : MonoBehaviour
 {
-    public GameObject foot;
+    [HideInInspector]
+    public KeyCode downCode;
+    public HingeJoint tempHinge = null;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Trigger on " + other.name);
-        if (other.tag == "Ground")
+        if (collision.collider.tag == "Ground")
         {
             Debug.Log("Trigger on Ground ");
-            //HingeJoint tempHinge = foot.AddComponent<HingeJoint>();
-            //tempHinge.anchor = foot.transform.InverseTransformPoint(transform.position);
+            if (Input.GetKey(downCode) && tempHinge == null)
+            {
+                tempHinge = gameObject.AddComponent<HingeJoint>();
+                tempHinge.anchor = transform.InverseTransformPoint(collision.GetContact(0).point);
+                tempHinge.connectedAnchor = collision.GetContact(0).point;
+                tempHinge.axis = Vector3.forward;
+                tempHinge.useMotor = true;
+                tempHinge.useLimits = true;
+                JointLimits l = new JointLimits();
+                l.min = -70;
+                l.max = 70;
+                tempHinge.limits = l;
+                JointMotor m = new JointMotor();
+                m.targetVelocity = 300;
+                m.force = 1000;
+                tempHinge.motor = m;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(downCode) && tempHinge !=null)
+        {
+            Destroy(tempHinge);
+            tempHinge = null;
         }
     }
 }
