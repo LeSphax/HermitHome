54283cfc5a5ab2a62d23e1a7763036ee034a58a6
Sebastien Kerbrat <sbkerbrat@gmail.com>
2019-01-26 19:22:38 +0100

    Add camera following and text at the top

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    private Transform personalTransform;
+
+
+    private void Awake()
+    {
+        personalTransform = new GameObject().transform;
+    }
+
+    private void FixedUpdate()
+    {
+        personalTransform = target.transform;
+        personalTransform.rotation = Quaternion.Euler(0, target.transform.rotation.eulerAngles.y, 0);
+        Vector3 targetPoint = personalTransform.TransformPoint(15, 15, 15);
+        targetPoint.y = 15;
+        transform.position = Vector3.Lerp(transform.position, personalTransform.TransformPoint(20, 20, 0), 0.01f);
+    }
+}
---
+++ Assets/TopScreenText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TopScreenText : MonoBehaviour
+{
+    public Text text;
+    public static Text _text;
+
+    private void Awake()
+    {
+        _text = text;
+    }
+
+    public static void SetText(string content)
+    {
+        _text.text = content;
+    }
+}
